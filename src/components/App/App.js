19b1b858dd35d4e8579481427da5a68a223158d2
danny mayasich <dmayasich08@gmail.com>
2019-01-14 13:00:10 -0800

    building components

---
+++ src/components/App/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from '../components/NavBar/NavBar';
+import MainContent from '';
+import Footer from '';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  // handleLogout
+
+  // handleSignupOrLogin
+
+  // handleClick
+
+  // handleNewComment
+
+  // --------- Lifecycle Methods --------- //
+
+  render() {
+    return (
+      <NavBar/>
+      <MainContent/>
+      <Footer/>
+      <
+    );
+  }
+}
+
+export default App;
