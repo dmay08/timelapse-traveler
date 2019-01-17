c027377db031ae250990f445d1fdb343f95eb596
danny mayasich <dmayasich08@gmail.com>
2019-01-17 14:29:43 -0800

    building architecture more

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,16 +5,33 @@ const Main = (props) => {
     return (
         <div id="main">
             <div id="one"></div>
+
             <div id="wrap">
                 <div id="two"></div>
             </div>
+
             <div id="outer-video-wrap">
                 <div id="inner-video-wrap">
-                    <div id ="a" className="videos"></div>
-                    <div id ="b" className="videos"></div>
-                    <div id ="c" className="videos"></div>
+                    <div id ="a" class="videos">
+                        <div class="video"></div>
+                        <div class="description"></div>
+                    </div>
+                    <div id ="b" class="videos">
+                        <div class="video"></div>
+                        <div class="description"></div>
+                    </div>
+                    <div id ="c" class="videos">
+                        <div class="video"></div>
+                        <div class="description"></div>
+                    </div>
                 </div>
             </div>
+
+            <div id="big-videos-wrap">
+                <div id="big-1"></div>
+                <div id="big-1-description"></div>
+            </div>
+  
         </div>
     );
 }
