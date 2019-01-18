e6ccd72bef7664265044959ac65947899739fe79
danny mayasich <dmayasich08@gmail.com>
2019-01-17 19:44:17 -0800

    architecture over halfway complete

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,31 +6,29 @@ const Main = (props) => {
         <div id="main">
             <div id="one"></div>
 
-            <div id="wrap">
-                <div id="two"></div>
-            </div>
+<div id="wrap">
+  <div id="two"></div>
+</div>
 
-            <div id="outer-video-wrap">
-                <div id="inner-video-wrap">
-                    <div id ="a" class="videos">
-                        <div class="video"></div>
-                        <div class="description"></div>
-                    </div>
-                    <div id ="b" class="videos">
-                        <div class="video"></div>
-                        <div class="description"></div>
-                    </div>
-                    <div id ="c" class="videos">
-                        <div class="video"></div>
-                        <div class="description"></div>
-                    </div>
-                </div>
-            </div>
+<div id="outer-video-wrap">
+  <div id ="a" class="video-wrap">
+    <div class="video"></div>
+    <div class="description"></div>
+  </div>
+  <div id ="b" class="video-wrap">
+    <div class="video"></div>
+    <div class="description"></div>
+  </div>
+  <div id ="c" class="video-wrap">
+    <div class="video"></div>
+    <div class="description"></div>
+  </div>
+</div>
 
-            <div id="big-videos-wrap">
-                <div id="big-1"></div>
-                <div id="big-1-description"></div>
-            </div>
+<div id="big-videos-wrap">
+  <div id="big-1"></div>
+  <div id="big-1-description"></div>
+</div>
   
         </div>
     );
