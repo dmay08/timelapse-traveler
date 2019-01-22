5abfaccc88065ed99524a1a978f6c6fbdcee8805
danny mayasich <dmayasich08@gmail.com>
2019-01-22 14:07:07 -0800

    titles complete

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ const Main = (props) => {
 
 <div id="wrap">
   <div id="my-info"></div>
-  <div id="title-wrap">
+  <div class="title-wrap">
     <div class="title-line" id="title-line-left"></div>
     <h1 id="recent-video-text">Recent Videos</h1>
     <div class="title-line" id="title-line-right"></div>
@@ -16,18 +16,30 @@ const Main = (props) => {
 
 </div>
 
-<div id="outer-video-wrap">
-  <div id ="a" class="video-wrap">
-    <div class="video"></div>
-    <div class="description"></div>
+<div id="recent-video-wrap">
+  <div id="outer-video-wrap">
+    
+    <div id ="a" class="video-wrap">
+      <div class="video"></div>
+      <div class="description"></div>
+    </div>
+    
+    <div id ="b" class="video-wrap">
+      <div class="video"></div>
+      <div class="description"></div>
+    </div>
+    
+    <div id ="c" class="video-wrap">
+      <div class="video"></div>
+      <div class="description"></div>
+    
+    </div>
+  
   </div>
-  <div id ="b" class="video-wrap">
-    <div class="video"></div>
-    <div class="description"></div>
-  </div>
-  <div id ="c" class="video-wrap">
-    <div class="video"></div>
-    <div class="description"></div>
+  <div class="title-wrap" id="compilation-wrap">
+    <div class="title-line" id="title-line-left"></div>
+    <h1 id="recent-video-text">Compilations</h1>
+    <div class="title-line" id="title-line-right"></div>
   </div>
 </div>
 
