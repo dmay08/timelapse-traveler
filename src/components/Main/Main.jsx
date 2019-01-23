7bcc2121eb12a3a8844ff55594185d15b09cf827
danny mayasich <dmayasich08@gmail.com>
2019-01-22 19:16:14 -0800

    about me complete, layout fixed so recent videos and compilations align

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,53 +1,78 @@
 import React from 'react';
 import './Main.css';
+import my_pic from '../../Images/my_pic.jpg';
+import taipei from '../../Images/taipei.jpg';
 
 const Main = (props) => {
     return (
         <div id="main">
-            <div id="one"></div>
+            <div id="one">
+              <img id="taipei" src={taipei}/>
+            </div>
 
-<div id="wrap">
-  <div id="my-info"></div>
-  <div class="title-wrap">
-    <div class="title-line" id="title-line-left"></div>
-    <h1 id="recent-video-text">Recent Videos</h1>
-    <div class="title-line" id="title-line-right"></div>
-  </div>
+            <div id="wrap">
+              <div id="my-info">
+                <div id="my-pic-wrap">
+                  <img id="my-pic" src={my_pic}/>
+                </div>
+                <div>
+                  <div id="about-me">
+                    <h2>Welcome!</h2>
+                    <p>My name is Danny and I'm a little bit obsessed with shooting 
+                      time-lapse videos. This past summer, my wife Noelle and I 
+                      traveled the world to say our marriage vows and we were able
+                      to see some extraordinary places. When we got back to the U.S.,
+                      we packed our bags again and moved to Los Angeles. 
+                      I built this website
+                      to share some of our adventures (home and abroad). If you like what you see,
+                      check me out on instagram @lapsed_traveler. Thanks for
+                      stopping by!
+                    </p>
+                    <br></br>
+                    <p>-Danny</p>
+                  </div>
+                </div>
+              </div>
+              <div className="title-wrap">
+                <div className="title-line" id="title-line-left"></div>
+                <h1 id="recent-video-text">Recent Videos</h1>
+                <div className="title-line" id="title-line-right"></div>
+              </div>
+            </div>
 
-</div>
+            <div id="recent-video-wrap">
+              <div id="outer-video-wrap">
+                
+                <div id ="a" class="video-wrap">
+                  <div className="video"></div>
+                  <div className="description"></div>
+                </div>
+                
+                <div id ="b" className="video-wrap">
+                  <div className="video"></div>
+                  <div className="description"></div>
+                </div>
+                
+                <div id ="c" className="video-wrap">
+                  <div className="video"></div>
+                  <div className="description"></div>
+                </div>
+              
+              </div>
+            </div>
 
-<div id="recent-video-wrap">
-  <div id="outer-video-wrap">
-    
-    <div id ="a" class="video-wrap">
-      <div class="video"></div>
-      <div class="description"></div>
-    </div>
-    
-    <div id ="b" class="video-wrap">
-      <div class="video"></div>
-      <div class="description"></div>
-    </div>
-    
-    <div id ="c" class="video-wrap">
-      <div class="video"></div>
-      <div class="description"></div>
-    
-    </div>
-  
-  </div>
-  <div class="title-wrap" id="compilation-wrap">
-    <div class="title-line" id="title-line-left"></div>
-    <h1 id="recent-video-text">Compilations</h1>
-    <div class="title-line" id="title-line-right"></div>
-  </div>
-</div>
-
-<div id="big-videos-wrap">
-  <div id="big-1"></div>
-  <div id="big-1-description"></div>
-</div>
-  
+            <div className="title-wrap" id="compilation-wrap">
+              <div className="title-line" id="title-line-left"></div>
+              <h1 id="recent-video-text">Compilations</h1>
+              <div className="title-line" id="title-line-right"></div>
+            </div>
+            
+            <div id="outer-large-wrap">
+              <div id="inner-large-wrap">
+                <div id="big-1"></div>
+                <div id="big-1-description"></div>
+              </div>
+            </div>  
         </div>
     );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './NavBar.css';
 
+
 const NavBar = (props) => {
     return (
-        <div className='NavBar'>
-            <p>Time(lapse) Traveler</p>
+        <div className='NavBar-wrap'>
+            <div className='NavBar'>
+                <div className="nav-thirds" id="first">
+                    <p>T / T</p>
+                </div>
+                <div className="nav-thirds" id="second">
+                    <p>Time(lapse) Traveler</p>
+                </div>
+                <div className="nav-thirds" id="third">
+                </div>
+            </div>
         </div>
     );
 }
