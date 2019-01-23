0a05f9ecc16cfa4377666bf09e611089694bbe3e
danny mayasich <dmayasich08@gmail.com>
2019-01-22 21:08:40 -0800

    recent videos added and autoplay

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -44,17 +44,23 @@ const Main = (props) => {
               <div id="outer-video-wrap">
                 
                 <div id ="a" class="video-wrap">
-                  <div className="video"></div>
+                  <div className="video">
+                    <iframe width="347" height="200" src="https://www.youtube.com/embed/LAIXZ4evg5M?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
+                  </div>
                   <div className="description"></div>
                 </div>
                 
                 <div id ="b" className="video-wrap">
-                  <div className="video"></div>
+                  <div className="video">
+                  <iframe width="347" height="200" src="https://www.youtube.com/embed/UceBHNagqck?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
                   <div className="description"></div>
                 </div>
                 
                 <div id ="c" className="video-wrap">
-                  <div className="video"></div>
+                  <div className="video">
+                  <iframe width="347" height="200" src="https://www.youtube.com/embed/tzMVczq280Q?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
                   <div className="description"></div>
                 </div>
               
